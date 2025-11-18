5d8b201aa4f68e71ebe58901ae5bde7add14236c
Moses Maina <mainacalton57@gmail.com>
2025-11-18 13:05:14 +0300

    lab complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
-#!/usr/bin/env python3
+# lib/sequences.py
 
 def print_fibonacci(length):
-    pass
+    """
+    Prints a list of Fibonacci numbers up to the given length.
+    """
+    if length <= 0:
+        print([])
+        return
+
+    fib_sequence = [0, 1]  # Start with the first two Fibonacci numbers
+
+    # Generate Fibonacci numbers until the list reaches the requested length
+    while len(fib_sequence) < length:
+        next_value = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_value)
+
+    # Slice the list if length is 1 (to return [0])
+    print(fib_sequence[:length])
